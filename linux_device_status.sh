c74155412273942791a7225ab5596b6007fd8160
Filip <95079889+v-filip@users.noreply.github.com>
2021-11-27 20:48:35 +0100

    Name change + minor code change
    Adding -W flag to ping so it doesn't hang for too long when ICMP packet doesn't return. 
    
    Adding the ability to pass a third parameter for $ sleep
    
    Changing the name because I'm adding a Mac specific script due to different paths

--- linux_device_status.sh
+++ linux_device_status.sh
@@ -3,19 +3,19 @@
 #Purpose: Checks the device status.
 
 function ping_loop () {
-	while ping -c 1 $1 > /dev/null
+	while ping -c 1 -W 1 $1 > /dev/null
 	do
 		local DATE=$(date)
 		echo "$2 is UP! | $DATE" >> /home/$USER/device_status.log
-		sleep 900 #15 minutes
+		sleep $3
 	done
 	echo "$2 is DOWN! Check the connection!" >> /home/$USER/device_status.log
-	sleep 60 #1 minute
+	sleep $3
 }
 
 touch /home/$USER/device_status.log
 
 while true
 do
-	ping_loop $1 $2
+	ping_loop $1 $2 $3
 done
